e0665782718c1a504aface44c5feae2c5bb133c7
CB_ATL_Cohort_2_111 <delinabpond@gmail.com>
2018-10-11 22:56:36 -0400

    made divs for game button, added colors to divs, declared vars for divs in js

---
+++ app.js
@@ -0,0 +1,11 @@
+let colorArr = document.getElementsByClassName('gameButton');
+let green = document.getElementById('green');
+let red = document.getElementById('red');
+let blue = document.getElementById('blue');
+let yellow = document.getElementById('yellow');
+let start = document.getElementById('startButton')
+
+
+
+
+
