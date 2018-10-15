e63d8ec83b1551feea3ffc1ba240a1f28fa5163e
CB_ATL_Cohort_2_111 <delinabpond@gmail.com>
2018-10-15 02:51:29 -0400

    game lights, no user functionality

--- app.js
+++ app.js
@@ -3,44 +3,79 @@ let green = document.getElementById('green');
 let red = document.getElementById('red');
 let blue = document.getElementById('blue');
 let yellow = document.getElementById('yellow');
-let start = document.getElementById('startButton')
+let start = document.getElementById('startButton');
+let counter = document.getElementById('roundCounter');
+let on = document.getElementById('turnOn')
 
+let interval; 
 let playerMoves = [];
 let moves = [];
+let lightOrder;
+flash = 0;
+turn = 1;
 
-
-// start button event 
+// start button event begins game and logs the counter
 
 start.addEventListener('click', function () {
-        startGame()
+    counter.innerHTML = "01";    
+    startGame()
 })
 
+// function to begin the game that pushes number array into the moves made to be followed by the player 
 function startGame() {
-
+  
     for (let x = 0; x < 15; x++){
         moves.push(Math.floor(Math.random() * 4) + 1)
+        console.log(moves[x])
+       
     }
-    console.log(moves)
-        setInterval(compMoves, 2000)
-}
+    lightOrder = true;
 
-
-function compMoves () {
-    if (moves[1] === moves[1]){
-        topLeft();
-    } 
-    if (moves[2] === moves[2]) {
-        topRight();
-    }
-    if (moves[3] === moves[3]){
-        bottomLeft();
-    } 
-    if (moves[4] === moves[4]){
-        bottomRight();
-    } 
+    interval = setInterval(compMoves, 2000)
     
 }
 
+// function to define the number array and change background style called through functions 
+
+function compMoves () {
+
+
+    if (flash == turn) {
+        clearInterval(interval);
+        lightOrder = false;
+        clearLights();
+      }
+
+    if (lightOrder) {
+            clearLights();
+    setTimeout ( function () {
+    if (moves[flash] == 1){
+        topLeft();
+        
+        
+    } 
+    if (moves[flash] == 2) {
+        topRight();
+        
+       
+    }
+    if (moves[flash] == 3){
+        bottomLeft();
+        
+        
+    } 
+    if (moves[flash] == 4){
+        bottomRight();
+        
+      
+
+    }
+    flash++;
+    }, 800);
+  }
+}
+
+// created functions to background the light divs to be called in previous function compMoves(line 40)
 function topLeft () {
     green.style.backgroundColor = "green"
 }
@@ -56,3 +91,14 @@ function bottomLeft () {
 function bottomRight () {
     blue.style.backgroundColor = "blue"
 }
+
+// function to change game light colors back to original (styled strangely for easy visibility, will update later)
+function clearLights () {
+    green.style.backgroundColor = "orange"
+    red.style.backgroundColor = "purple"
+    blue.style.backgroundColor = "black"
+    yellow.style.backgroundColor = "white"
+
+}
+
+
